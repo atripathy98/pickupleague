52567410beff6fd12c67eda47e234fb0170a33c9
Animesh Tripathy <a.tripathy101@gmail.com>
2018-10-26 01:11:05 -0400

    send new members message

--- app.js
+++ app.js
@@ -3,24 +3,27 @@
  *  ANIMESH TRIPATHY
  */
 
-BOT_TOKEN = "NTA1MjIxNzQ3NDM2NTUyMjMy.DrQcXQ.C_JgPstt1ows_brG48WQ5RLmtG4";
+TOKEN = "NTA1MjIxNzQ3NDM2NTUyMjMy.DrQcXQ.C_JgPstt1ows_brG48WQ5RLmtG4";
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Pick-Up League of Legends application running...");
 });
 
-// ping -> pong
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+// Create an event listener for new members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'welcome');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the Pick-Up League, ${member}. Make sure to check out #info.`);
 });
 
 // Login to Discord
-client.login(BOT_TOKEN);
+client.login(TOKEN);
 
 // app.set('port', (process.env.PORT || 5000));
 // app.use(express.static(__dirname + '/views'));
