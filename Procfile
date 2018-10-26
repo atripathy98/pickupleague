051a1206a830120e5d4e0f98fdbc78da5964f367
Animesh Tripathy <a.tripathy101@gmail.com>
2018-10-26 18:06:55 -0400

    converting code to python

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node app.js
+worker: python bot.py
---
+++ bot.py
@@ -0,0 +1,28 @@
+# ---------------------------
+#  DISCORD APPLICATION SERVER 
+#  ANIMESH TRIPATHY
+# ---------------------------
+import discord
+
+TOKEN = "NTA1MjIxNzQ3NDM2NTUyMjMy.DrQcXQ.C_JgPstt1ows_brG48WQ5RLmtG4"
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
