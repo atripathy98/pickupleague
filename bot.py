3693c38bc0f35605375785779c69baa0289dbc97
Animesh Tripathy <a.tripathy101@gmail.com>
2018-10-26 18:18:05 -0400

    converting code to python

--- bot.py
+++ bot.py
@@ -3,10 +3,13 @@
 #  ANIMESH TRIPATHY
 # ---------------------------
 import discord
+#from discord.ext import commands
 
 TOKEN = "NTA1MjIxNzQ3NDM2NTUyMjMy.DrQcXQ.C_JgPstt1ows_brG48WQ5RLmtG4"
 
 client = discord.Client()
+
+#bot = commands.Bot(command_prefix='$')
 
 @client.event
 async def on_message(message):
