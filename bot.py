9552a796578147d61a645386047d65fe4d68e250
Animesh Tripathy <a.tripathy101@gmail.com>
2018-10-26 18:34:12 -0400

    minor changes

--- bot.py
+++ bot.py
@@ -11,8 +11,8 @@ client = discord.Client()
 
 #bot = commands.Bot(command_prefix='$')
 
-@client.event
-async def on_message(message):
+@client.async_event
+def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
@@ -21,8 +21,8 @@ async def on_message(message):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
-@client.event
-async def on_ready():
+@client.async_event
+def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
